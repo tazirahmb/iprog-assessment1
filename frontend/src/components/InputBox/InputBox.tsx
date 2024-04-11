04ad3753ac95e52f1faca6782745801f85d716a9
tazirahmb <tazirahmb@outlook.com>
2024-04-11 15:23:37 +1000

    complete checkout form basic validation

--- frontend/src/components/InputBox/InputBox.tsx
+++ frontend/src/components/InputBox/InputBox.tsx
@@ -1,13 +1,25 @@
-export default function InputBox({ label, name, ...props }: InputBoxProps) {
-	return (
-		<>
-			<label htmlFor={name}>{label}</label>
-			<input name={name} id={name} aria-label={label} {...props} />
-		</>
-	);
-}
+'use client';
+
+import React from 'react';
+
+import style from './InputBox.module.css';
+
+const InputBox = React.forwardRef(
+	({ label, name, errorMessage, ...props }: InputBoxProps, ref) => {
+		return (
+			<div className={`col-6 ${style['input-box']}`}>
+				<label htmlFor={name}>{label}</label>
+				<input name={name} id={name} aria-label={label} ref={ref} {...props} />
+				<span style={{ color: 'red' }}>{errorMessage?.message}</span>
+			</div>
+		);
+	},
+);
+
+export default InputBox;
 
 interface InputBoxProps {
 	label: string;
 	name: string;
+	errorMessage: object | null;
 }
--- frontend/src/containers/CheckOut/CheckOut.tsx
+++ frontend/src/containers/CheckOut/CheckOut.tsx
@@ -1,34 +1,129 @@
+'use client';
+
 // component
-import Header from '@/components/Header';
+// import Header from '@/components/Header';
 import InputBox from '@/components/InputBox/InputBox';
-import Link from 'next/link';
+// import Link from 'next/link';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object({
+	firstName: yup.string().required('This field is Required'),
+	lastName: yup.string().required('This field is Required'),
+	phoneNumber: yup
+		.number()
+		.min(10, 'Phone number must be at 10 digit')
+		.max(10, 'Phone number must be at 10 digit')
+		.required('This field is Required'),
+	email: yup.string().email().required('This field is Required'),
+	address: yup.string().required('This field is Required'),
+	state: yup.string().required('This field is Required'),
+	suburb: yup.string().required('This field is Required'),
+	postcode: yup
+		.number()
+		.min(4, 'Postal code must be at 4 digit')
+		.max(4, 'Postal code must be at 4 digit')
+		.required('This field is Required'),
+});
+
+import { useForm } from 'react-hook-form';
+
+import style from './CheckOut.module.css';
+
+const stateData = ['NSW', 'VIC', 'QLD', 'WA', 'SA', 'TAS', 'ACT', 'NT'];
 
 export default function CheckOut() {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+	console.log(errors);
+	const onSubmit = (data) => console.log(data);
+
+	console.log(watch('firstName', { required: true }));
+
 	return (
 		<>
-			<div>Checkout page</div>
-			<InputBox label="First Name" name="firstName" />
-			<InputBox label="Last Name" name="lastName" />
-			<InputBox label="Mobile Number" name="mobileNumber" />
-			<InputBox label="Email" name="email" />
-			<p>
-				<strong>Address Information</strong>
-			</p>
-			<InputBox label="Address" name="address" />
-			<InputBox label="Suburb" name="suburb" />
-			{/* Should be dropdown */}
-			<InputBox label="State" name="state" />
-			<InputBox label="Postcode" name="postcode" />
-			<Link
-				href="/order-complete"
-				style={{
-					backgroundColor: 'yellow',
-					fontWeight: 'bold',
-					padding: '8px 16px',
-				}}
-			>
-				Submit
-			</Link>
+			<main className="container mb-8">
+				<h1 className={`${style['header']} mb-3`}>
+					Checkout page / Delivery Detail
+				</h1>
+				<div className="flex-row justify-content-center">
+					<div className="col-6">
+						<form onSubmit={handleSubmit(onSubmit)} className="flex-row g-2">
+							<InputBox
+								label="First Name"
+								{...register('firstName', { required: true })}
+								errorMessage={errors.firstName}
+							/>
+							<InputBox
+								label="Last Name"
+								{...register('lastName', { required: true })}
+								errorMessage={errors.lastName}
+							/>
+							<InputBox
+								label="Mobile Number"
+								{...register('phoneNumber', {
+									required: true,
+								})}
+								errorMessage={errors.phoneNumber}
+							/>
+							<InputBox
+								label="Email"
+								{...register('email', { required: true })}
+								errorMessage={errors.email}
+							/>
+							<h2 className="col-12">
+								<strong>Address Information</strong>
+							</h2>
+							<InputBox
+								label="Address"
+								{...register('address', { required: true })}
+								errorMessage={errors.address}
+							/>
+							<InputBox
+								label="Suburb"
+								{...register('suburb', { required: true })}
+								errorMessage={errors.suburb}
+							/>
+							{/* Should be dropdown */}
+							<InputBox
+								label="State"
+								list="state-list"
+								{...register('state', { required: true })}
+								errorMessage={errors.state}
+							/>
+							{/* datalist of state */}
+							<datalist id="state-list">
+								{stateData.map((state) => (
+									<option value={state}></option>
+								))}
+							</datalist>
+							<InputBox
+								label="Postcode"
+								{...register('postcode', { required: true })}
+								errorMessage={errors.postcode}
+							/>
+							<button
+								// disabled={errors}
+								className="col-12"
+								style={{
+									backgroundColor: 'yellow',
+									fontWeight: 'bold',
+									padding: '8px 16px',
+									textAlign: 'center',
+								}}
+							>
+								Submit
+							</button>
+						</form>
+					</div>
+				</div>
+			</main>
 		</>
 	);
 }
